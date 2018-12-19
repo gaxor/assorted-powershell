57f47aa42645d2c44040072339eeaea84df0b5be
Gogs <gogs@fake.local>
2018-12-19 16:29:08 -0700

    Add scenarios

--- Sandbox/Compare [void] vs out-null.ps1
+++ Sandbox/Compare [void] vs out-null.ps1
@@ -5,3 +5,28 @@ $data = New-Object byte[]( 5000 )
 
 Measure-TMCommand -Command {$data | out-null} -Repetitions 50 | select -Last 1 -ExpandProperty AverageMilliseconds
 Measure-TMCommand -Command {[void]$data} -Repetitions 50 | select -Last 1 -ExpandProperty AverageMilliseconds
+
+# More tests with built-in measure-command
+Clear-Variable data
+Measure-Command {
+    $data = New-Object byte[](5000)
+    $data | out-null
+} | select -expand totalmilliseconds | write-host -fore yel
+
+Clear-Variable data
+Measure-Command {
+    $data = New-Object byte[](5000)
+    [void]$data
+} | select -expand totalmilliseconds | write-host -fore yel
+
+Clear-Variable data
+Measure-Command {
+    $data = New-Object byte[](5000)
+    $data = $null
+} | select -expand totalmilliseconds | write-host -fore yel
+
+Clear-Variable data
+Measure-Command {
+    $data = New-Object byte[](5000)
+    Clear-Variable data
+} | select -expand totalmilliseconds | write-host -fore yel
